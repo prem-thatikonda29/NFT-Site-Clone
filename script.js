24cb1f729c7b192458256dacc072d901a2e07e51
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-03-23 17:37:54 +0530

    scroll effects added

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const counters = document.querySelector('.hero-counters')
 const hero_details = document.querySelector('.hero-details')
 const hero_buttons = document.querySelector('.hero-buttons')
-
+const hero_image = document.querySelector('.hero_image')
 
 document.addEventListener('scroll', function() {
     let value = window.scrollY
 
     counters.style.marginLeft = -value * 0.4 + 'px'
-    hero_details.style.marginLeft = -value * 0.4 + 'px'
-    hero_buttons.style.marginLeft = -value * 0.4 + 'px'
+    hero_details.style.marginLeft = -value * 0.8 + 'px'
+    hero_buttons.style.marginLeft = -value * 0.6 + 'px'
+    hero_image.style.marginLeft = value * 0.6 + 'px'
 
     console.log(value);
 })
